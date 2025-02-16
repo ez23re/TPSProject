853fdbc1f0b8e64a298691b46dc98d50be7f518b
ez23re <ez23re@gmail.com>
2025-02-16 21:00:02 +0900

    02/11 수업까지

---
+++ Source/TPSProject/TPSPlayerAnimInstance.cpp
@@ -0,0 +1,17 @@
+﻿#include "TPSPlayerAnimInstance.h"
+#include "TPSCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UTPSPlayerAnimInstance::NativeUpdateAnimation ( float DeltaSeconds )
+{
+	Super::NativeUpdateAnimation ( DeltaSeconds );
+
+	ATPSCharacter* player = Cast<ATPSCharacter> ( TryGetPawnOwner ( ) );
+	if (!player) return;
+
+	FVector velocity = player->GetVelocity ( );
+	FVector forwardVector = player->GetActorForwardVector ( );
+
+	Speed = FVector::DotProduct ( velocity , forwardVector );
+
+}
---
+++ Source/TPSProject/TPSPlayerAnimInstance.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TPSPlayerAnimInstance.generated.h"
+
+UCLASS()
+class TPSPROJECT_API UTPSPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	virtual void NativeUpdateAnimation ( float DeltaSeconds ) override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=TPSPlayerAnimInstance)
+	float Speed = 0.f;
+
+};
